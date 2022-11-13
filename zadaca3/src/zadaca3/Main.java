bec151179449c3f8d4c819d435061d0ba0e247d8
unknown <zigicpetar12@gmail.com>
2022-11-13 15:27:01 +0100

    commit za vezbite

---
+++ zadaca3/src/zadaca3/Main.java
@@ -0,0 +1,14 @@
+package zadaca3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        PC pc1 = new PC();
+		
+		pc1.prvMetod(4,500);
+
+
+	}
+
+}
